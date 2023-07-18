8f8cd5d72b6e04a0d924746289afdb165e9abef2
fat83dotcom <fernando.quimic@gmail.com>
2023-07-18 17:58:27 -0300

    Menu de opções.

---
+++ codigosEmPython/BinaryTrees.py
@@ -0,0 +1,177 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+
+class Node:
+    def __init__(self, key, day, month, year, nSunspot) -> None:
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+        self.day = day
+        self.month = month
+        self.year = year
+        self.nSunspot = nSunspot
+
+    def __repr__(self) -> str:
+        data = f'Key: {self.key} --- '\
+            f'{self.day}/{self.month}/{self.year} -> Spots: {self.nSunspot}'
+        return data
+
+
+class Avl:
+    def __init__(self) -> None:
+        self.node = None
+
+    def insert(self, key, day, month, year, nSunspot):
+        if not self.node:
+            self.node = Node(key, day, month, year, nSunspot)
+        else:
+            self.node = self._insertNode(
+                self.node, key, day, month, year, nSunspot
+            )
+
+    def _insertNode(self, node: Node, key, day, month, year, nSunspot) -> Node:
+        if not node:
+            return Node(key, day, month, year, nSunspot)
+        elif key < node.key:
+            node.left = self._insertNode(
+                node.left, key, day, month, year, nSunspot
+            )
+        else:
+            node.right = self._insertNode(
+                node.right, key, day, month, year, nSunspot
+            )
+
+        node.height = 1 + max(
+            self._height(node.left), self._height(node.right)
+        )
+        balance = self.balanceFactor(node)
+
+        if balance > 1 and key < node.left.key:
+            return self._rotationRight(node)
+        if balance < -1 and key > node.right.key:
+            return self._rotationLeft(node)
+        if balance > 1 and key > node.left.key:
+            node.left = self._rotationLeft(node.left)
+            return self._rotationRight(node)
+        if balance < -1 and key < node.right.key:
+            node.right = self._rotationRight(node.right)
+            return self._rotationLeft(node)
+
+        return node
+
+    def _height(self, node: Node) -> int:
+        if not node:
+            return 0
+        return node.height
+
+    def balanceFactor(self, node: Node) -> int:
+        if not node:
+            return 0
+        return self._height(node.left) - self._height(node.right)
+
+    def _rotationRight(self, nodeZ: Node) -> Node:
+        nodeY: Node = nodeZ.left
+        nodeT3: Node = nodeY.right
+
+        nodeY.right = nodeZ
+        nodeZ.left = nodeT3
+
+        nodeZ.height = 1 + max(
+            self._height(nodeZ.left), self._height(nodeZ.right)
+        )
+        nodeY.height = 1 + max(
+            self._height(nodeY.left), self._height(nodeY.right)
+        )
+        return nodeY
+
+    def _rotationLeft(self, nodeZ: Node) -> Node:
+        nodeY: Node = nodeZ.right
+        nodeT2: Node = nodeY.left
+
+        nodeY.left = nodeZ
+        nodeZ.right = nodeT2
+
+        nodeZ.height = 1 + max(
+            self._height(nodeZ.left), self._height(nodeZ.right)
+        )
+        nodeY.height = 1 + max(
+            self._height(nodeY.left), self._height(nodeY.right)
+        )
+        return nodeY
+
+    def inorderItertive(self):
+        if self.node is None:
+            return
+
+        stack: list = []
+        node: Node = self.node
+
+        while True:
+            if node is not None:
+                stack.append(node)
+                node = node.left
+
+            elif stack:
+                node = stack.pop()
+                print(node)
+                node = node.right
+
+            else:
+                break
+
+    def preorderIterative(self) -> None:
+        if self.node is None:
+            return
+
+        stack: list = []
+        stack.append(self.node)
+
+        while stack:
+            node: Node = stack.pop()
+            print(node)
+
+            if node.right is not None:
+                stack.append(node.right)
+            if node.left is not None:
+                stack.append(node.left)
+
+    def searchKey(self, key) -> Node | None:
+        currentNode = self.node
+
+        while currentNode is not None:
+            if key == currentNode.key:
+                return currentNode
+            elif key < currentNode.key:
+                currentNode = currentNode.left
+            else:
+                currentNode = currentNode.right
+
+    def inorderRecursive(self, node: Node) -> None:
+        if node is not None:
+            self.inorderRecursive(node.left)
+            print(node)
+            self.inorderRecursive(node.right)
+
+    def searchRecursiveKey(self, node: Node, key: int) -> None:
+        if node.key == key or node is None:
+            return node
+        elif key < node.key:
+            return self.searchRecursiveKey(node.left, key)
+        else:
+            return self.searchRecursiveKey(node.right, key)
+
+    def minimum(self) -> Node:
+        currentNode: Node = self.node
+        while currentNode.left is not None:
+            currentNode = currentNode.left
+        return currentNode
+
+    def maximum(self) -> Node:
+        currentNode: Node = self.node
+        while currentNode.right is not None:
+            currentNode = currentNode.right
+        return currentNode
+
+    # def sucessor(self)
--- codigosEmPython/arvoresPython.py
+++ codigosEmPython/arvoresPython.py
@@ -1,120 +1,4 @@
-class Node:
-    def __init__(self, key, day, month, year, nSunspot) -> None:
-        self.key = key
-        self.left = None
-        self.right = None
-        self.height = 1
-        self.day = day
-        self.month = month
-        self.year = year
-        self.nSunspot = nSunspot
-
-    def __repr__(self) -> str:
-        data = f'{self.day}/{self.month}/{self.year} -> Spots: {self.nSunspot}'
-        return data
-
-
-class Avl:
-    def __init__(self) -> None:
-        self.__root = None
-
-    def insert(self, key, day, month, year, nSunspot):
-        if not self.__root:
-            self.__root = Node(key, day, month, year, nSunspot)
-        else:
-            self.__root = self._insertNode(
-                self.__root, key, day, month, year, nSunspot
-            )
-
-    def _insertNode(self, node: Node, key, day, month, year, nSunspot) -> Node:
-        if not node:
-            return Node(key, day, month, year, nSunspot)
-        elif key < node.key:
-            node.left = self._insertNode(
-                node.left, key, day, month, year, nSunspot
-            )
-        else:
-            node.right = self._insertNode(
-                node.right, key, day, month, year, nSunspot
-            )
-
-        node.height = 1 + max(
-            self._height(node.left), self._height(node.right)
-        )
-        balance = self.balanceFactor(node)
-
-        if balance > 1 and key < node.left.key:
-            return self._rotationRight(node)
-        if balance < -1 and key > node.right.key:
-            return self._rotationLeft(node)
-        if balance > 1 and key > node.left.key:
-            node.left = self._rotationLeft(node.left)
-            return self._rotationRight(node)
-        if balance < -1 and key < node.right.key:
-            node.right = self._rotationRight(node.right)
-            return self._rotationLeft(node)
-
-        return node
-
-    def _height(self, node: Node) -> int:
-        if not node:
-            return 0
-        return node.height
-
-    def balanceFactor(self, node: Node) -> int:
-        if not node:
-            return 0
-        return self._height(node.left) - self._height(node.right)
-
-    def _rotationRight(self, nodeZ: Node) -> Node:
-        nodeY: Node = nodeZ.left
-        nodeT3: Node = nodeY.right
-
-        nodeY.right = nodeZ
-        nodeZ.left = nodeT3
-
-        nodeZ.height = 1 + max(
-            self._height(nodeZ.left), self._height(nodeZ.right)
-        )
-        nodeY.height = 1 + max(
-            self._height(nodeY.left), self._height(nodeY.right)
-        )
-        return nodeY
-
-    def _rotationLeft(self, nodeZ: Node) -> Node:
-        nodeY: Node = nodeZ.right
-        nodeT2: Node = nodeY.left
-
-        nodeY.left = nodeZ
-        nodeZ.right = nodeT2
-
-        nodeZ.height = 1 + max(
-            self._height(nodeZ.left), self._height(nodeZ.right)
-        )
-        nodeY.height = 1 + max(
-            self._height(nodeY.left), self._height(nodeY.right)
-        )
-        return nodeY
-
-    def inorder(self):
-        if self.__root is None:
-            return
-
-        stack: list = []
-        current: Node = self.__root
-
-        while True:
-            if current is not None:
-                stack.append(current)
-                current = current.left
-
-            elif stack:
-                current = stack.pop()
-                print(current)
-                current = current.right
-
-            else:
-                break
+from BinaryTrees import Avl
 
 
 class ExecuteMain:
@@ -123,20 +7,89 @@ class ExecuteMain:
 
     def run(self) -> None:
         try:
+            print('Carregando árvore de busca binária...')
             dataTree = Avl()
             with open(self.path, 'r', encoding='utf-8') as file:
                 for n, x in enumerate(file.readlines()):
                     if int(x[21:24]) >= 0:
+                        year: int = int(x[:4].strip())
+                        month: int = int(x[5:8].strip())
+                        day: int = int(x[8:10].strip())
                         dataTree.insert(
                             n,
-                            year=int(x[:4].strip()),
-                            month=int(x[5:8].strip()),
-                            day=int(x[8:10].strip()),
+                            year=year,
+                            month=month,
+                            day=day,
                             nSunspot=int(x[21:24].strip())
                         )
-            dataTree.inorder()
+            print('\nCarregamento concluído !\n')
         except Exception as e:
             print(e)
+        menu = '''
+            Opções:
+
+            1 -> Percurso em-ordem Iterativo.
+            2 -> Pecurso pré-ordem Iterativo.
+            3 -> Percuroso em-ordem Recusivo.
+            4 -> Busca Recursiva.
+            5 -> Chave mínima.
+            6 -> Chave máxima.
+            7 -> Procure um intervalo de chaves.
+            0 -> Sair.
+            '''
+        while True:
+            try:
+                print(menu)
+                op = input('Digite uma opção: ')
+                if int(op) == 1:
+                    try:
+                        dataTree.inorderItertive()
+                    except Exception as e:
+                        raise e
+                if int(op) == 2:
+                    try:
+                        dataTree.preorderIterative()
+                    except Exception as e:
+                        raise e
+                if int(op) == 3:
+                    try:
+                        dataTree.inorderRecursive(dataTree.node)
+                    except Exception as e:
+                        raise e
+                if int(op) == 4:
+                    try:
+                        key = input('Digite um número inteiro: ')
+                        print(
+                            dataTree.searchRecursiveKey(
+                                dataTree.node, int(key)
+                            )
+                        )
+                    except Exception as e:
+                        raise e
+                if int(op) == 5:
+                    try:
+                        print(dataTree.minimum())
+                    except Exception as e:
+                        raise e
+                if int(op) == 6:
+                    try:
+                        print(dataTree.maximum())
+                    except Exception as e:
+                        raise e
+                if int(op) == 7:
+                    try:
+                        init = input('Digite um número inteiro/Início: ')
+                        end = input('Digite um número inteiro/Final: ')
+                        for i in range(int(init), int(end)):
+                            print(dataTree.searchKey(i))
+
+                    except Exception as e:
+                        raise e
+                if int(op) == 0:
+                    print('Até logo !')
+                    break
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
